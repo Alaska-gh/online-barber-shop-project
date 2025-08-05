9ca1fce106cd9d01038a6e3afa8bf7a4368b17ee
Alaska-gh <bashiruyussif@gmail.com>
2025-08-05 11:49:56 +0000

    installed and used swiper library to implement sliding for the styles section.

--- src/app/styles.component/styles.component.ts
+++ src/app/styles.component/styles.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { StylesService } from './styles.service';
 import { CommonModule } from '@angular/common';
+import Swiper from 'swiper';
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
+
+Swiper.use([Navigation, Pagination, Autoplay])
 
 @Component({
   selector: 'styles-component',
@@ -8,9 +12,34 @@ import { CommonModule } from '@angular/common';
   templateUrl: './styles.component.html',
   styleUrl: './styles.component.css'
 })
-export class StylesComponent {
+export class StylesComponent implements AfterViewInit{
+
   styles
   constructor(){
   this.styles = new StylesService()
   } 
+    ngAfterViewInit() {
+    new Swiper('.swiper-container', 
+      {
+        loop: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false
+        },
+        pagination:{
+          el: '.swiper-pagination',
+          clickable: true
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        breakpoints: {
+          640: { slidesPerView: 1, navigation: false },
+          768: { slidesPerView: 2, navigation: false},
+          1024: { slidesPerView: 4, navigation: true}
+        }
+      }
+    );
+  }
 }
