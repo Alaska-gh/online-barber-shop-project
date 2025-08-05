524f32f7f032eff668f9257a69519cfaf841fcd3
Alaska-gh <bashiruyussif@gmail.com>
2025-08-05 00:23:36 +0000

    created the about us component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header.component/header.component';
 import { HeroComponent } from './hero.component/hero.component';
 import { StylesComponent } from './styles.component/styles.component';
+import { AboutComponent } from './about.component/about.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, StylesComponent],
+  imports: [RouterOutlet,
+     HeaderComponent, 
+     HeroComponent, 
+     StylesComponent,
+     AboutComponent,
+     
+    ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
